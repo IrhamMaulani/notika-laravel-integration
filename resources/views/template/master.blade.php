f73f45a2baec1a0116558093b37122bbc805d36a
Irham Maulani <irhammaulani@gmail.com>
2019-06-02 21:04:39 +0800

    add javascript

--- resources/views/template/master.blade.php
+++ resources/views/template/master.blade.php
@@ -61,7 +61,7 @@
     <link href="{{ asset('css/responsive.css') }}" rel="stylesheet">
 
      @yield('css')
-     
+
     <!-- modernizr JS
 		============================================ -->
     <script src="{{asset('js/vendor/modernizr-2.8.3.min.js')}}"></script>
@@ -603,14 +603,13 @@
 
     {{-- Status Area/Header Area Of Content --}}
 
-     <div class="notika-status-area">
+     {{-- <div class="notika-status-area">
         <div class="container">
             <div class="row">
                 @yield('content-header')
                 </div>
         </div>
-    </div>
-
+    </div> --}}
 
     {{-- End Status Area/Header Area Of Content --}}
 
@@ -620,6 +619,7 @@
     {{-- content --}}
 
 
+    @yield('content')
 
     {{-- End Content --}}
 
@@ -631,7 +631,7 @@
             <div class="row">
                 <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
                     <div class="footer-copy-right">
-                       @yield('footer')
+                    @yield('footer')
                     </div>
                 </div>
             </div>
@@ -639,6 +639,96 @@
     </div>
 
      {{--End Footer --}}
+
+
+     {{-- Javascript --}}
+
+    <script src="{{asset('js/vendor/jquery-1.12.4.min.js')}}"></script>
+    <!-- bootstrap JS
+		============================================ -->
+    <script src="{{asset('js/bootstrap.min.js')}}"></script>
+    <!-- wow JS
+		============================================ -->
+    <script src="{{asset('js/wow.min.js')}}"></script>
+    <!-- price-slider JS
+		============================================ -->
+    <script src="{{asset('js/jquery-price-slider.js')}}"></script>
+    <!-- owl.carousel JS
+		============================================ -->
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="{{asset('js/jquery-price-slider.js')}}"></script>
+<!-- scrollUp JS
+    ============================================ -->
+    <script src="{{asset('js/jquery.scrollUp.min.js')}}"></script>
+<!-- meanmenu JS
+    ============================================ -->
+    <script src="{{asset('js/meanmenu/jquery.meanmenu.js')}}"></script>
+<!-- counterup JS
+    ============================================ -->
+
+    <script src="{{asset('js/counterup/jquery.counterup.min.js')}}"></script>
+
+    <script src="{{asset('js/counterup/waypoints.min.js')}}"></script>
+
+    <script src="{{asset('js/counterup/counterup-active.js')}}"></script>
+<!-- mCustomScrollbar JS
+    ============================================ -->
+    <script src="{{asset('js/scrollbar/jquery.mCustomScrollbar.concat.min.js')}}"></script>
+<!-- jvectormap JS
+    ============================================ -->
+    <script src="{{asset('js/jvectormap/jquery-jvectormap-2.0.2.min.js')}}"></script>
+
+    <script src="{{asset('js/jvectormap/jquery-jvectormap-world-mill-en.js')}}"></script>
+
+    <script src="{{asset('js/jvectormap/jvectormap-active.js')}}"></script>
+<!-- sparkline JS
+    ============================================ -->
+    <script src="{{asset('js/sparkline/jquery.sparkline.min.js')}}"></script>
+
+    <script src="{{asset('js/sparkline/sparkline-active.js')}}"></script>
+<!-- sparkline JS
+    ============================================ -->
+
+    <script src="{{asset('js/flot/jquery.flot.js')}}"></script>
+
+    <script src="{{asset('js/flot/jquery.flot.resize.js')}}"></script>
+
+    <script src="{{asset('js/flot/curvedLines.js')}}"></script>
+
+    <script src="{{asset('js/flot/flot-active.js')}}"></script>
+<!-- knob JS
+    ============================================ -->
+    <script src="{{asset('js/knob/jquery.knob.js')}}"></script>
+
+    <script src="{{asset('js/knob/jquery.appear.js')}}"></script>
+
+    <script src="{{asset('js/knob/knob-active.js')}}"></script>
+<!--  wave JS
+    ============================================ -->
+    <script src="{{asset('js/wave/waves.min.js')}}"></script>
+
+    <script src="{{asset('js/wave/wave-active.js')}}"></script>
+<!--  todo JS
+    ============================================ -->
+    <script src="{{asset('js/todo/jquery.todo.js')}}"></script>
+<!-- plugins JS
+    ============================================ -->
+    <script src="{{asset('js/plugins.js')}}"></script>
+<!--  Chat JS
+    ============================================ -->
+    <script src="{{asset('js/chat/moment.min.js')}}"></script>
+
+    <script src="{{asset('js/chat/jquery.chat.js')}}"></script>
+<!-- main JS
+    ============================================ -->
+    <script src="{{asset('js/main.js')}}"></script>
+<!-- tawk chat JS
+    ============================================ -->
+    <script src="{{asset('js/tawk-chat.js')}}"></script>
+
+
+
+     {{-- End Of Javascript --}}
 
 
 
